bee161e811eea1181af5b39dab1188f5ab2af376
Sujay Anjankar <sanjankar@infocepts.com>
2018-10-12 11:00:49 +0800

    Finished code.

--- raspberry-pi/capture_image.py
+++ raspberry-pi/capture_image.py
@@ -1,6 +1,5 @@
 '''
 Created on Oct 9, 2018
-
 @author: sanjankar
 '''
 
@@ -9,30 +8,70 @@ import picamera
 from sense_hat import SenseHat
 from paramiko import SSHClient
 from paramiko import AutoAddPolicy
+from push_sensor_data import trigger
+from push_sensor_data import stop_data_push
 
-def take_picture():
+sense = SenseHat()
+picture_taken = False
+stop_run = False
+
+def take_picture(picture_taken):
     with picamera.PiCamera() as camera:
         camera.resolution = (1024, 768)
-        camera.start_preview()
-        time.sleep(2)
+        camera.rotation = (180)
         image_file_name = 'user_' + str(int(round(time.time() * 1000))) + '.jpg'
-        camera.capture(image_file_name)
-        copy_image(image_file_name)
-        
+        sense.show_letter('A', text_colour = [255, 0, 0])
+        print('Image: ' + image_file_name)
+        while True: 
+            for event in sense.stick.get_events():
+                if event.action == 'released' and event.direction == 'up':
+                    print("The joystick was {} {}".format(event.action, event.direction))
+                    camera.capture(image_file_name)
+                    picture_taken = True
+                    #copy_image(image_file_name) -  removed because showing error
+                    sense.show_letter('<', text_colour = [0, 255, 0])
+                    trigger(image_file_name,sense)
+                    return
+
 def copy_image(image_file_name):
     s = SSHClient()
-    destination_path = '/usr/local/tomcat/webapps/MicroStrategy/images' + image_file_name
+    destination_path = '/usr/local/tomcat/webapps/MicroStrategy/images/STELS/IOT/' + image_file_name
     s.set_missing_host_key_policy(AutoAddPolicy())
-    s.connect("10.10.5.170", 22, username="root", password="info@999", timeout=4)
+    s.connect("10.10.5.170", 22, username="root", password="info@999", timeout=10)
     sftp = s.open_sftp()
     print('Begin copying: ' + image_file_name + ' to: ' + destination_path)
     sftp.put(image_file_name, destination_path)
     print('Finished copying.')
     
-    
-sense = SenseHat()
+def stop_run():
+    sense.clear()
+    sense.show_letter('O', text_colour = [255, 0, 0])
+    stop_data_push()
+    return
+
+def start_run():
+    while picture_taken != True:
+        for event in sense.stick.get_events():
+            sense.show_letter('P', text_colour = [0, 100, 255])
+            if event.action == 'released' and event.direction == 'down':
+                print("The joystick was {} {}".format(event.action, event.direction))
+                take_picture(picture_taken)
+                return
+                
 while True:
-    for event in sense.stick.get_events():
-        print("The joystick was {} {}".format(event.action, event.direction))
-        if event.action == 'released' and event.direction == 'middle':
-            take_picture()
+    print("\n\n Beginning")
+    sense.show_letter('v', text_colour = [0, 255, 255])
+    time.sleep(0.1)
+##    sense.stick.direction_right = stop_run
+##    sense.stick.direction_middle = start_run
+    event= sense.stick.wait_for_event()
+    print("The joystick was {} {}".format(event.action, event.direction))
+    if event.direction == 'middle':
+        start_run()
+
+
+
+
+
+
+
--- raspberry-pi/push_sensor_data.py
+++ raspberry-pi/push_sensor_data.py
@@ -1,13 +1,7 @@
-'''
-Created on Sep 30, 2018
-
-@author: sanjankar
-'''
-
 import requests
 import base64
 import datetime
-import time
+import sched, time
 from sense_hat import SenseHat
 
 ### Parameters ###
@@ -18,8 +12,8 @@ project_id = '09CB082811E8A750096D0080EF45715A'
 base_url = "http://" + api_iserver + "/MicroStrategyLibrary/api/"
 datasetId = "D2529F2411E8C4A9343C0080EFE5BC8B" # Cube ID
 tableId = "sensor_data_table"
-driverId = "Ketan"
 sense = SenseHat()
+stop_run = False
 
 def login(base_url,api_login,api_password):
     print("Getting token...")
@@ -28,27 +22,28 @@ def login(base_url,api_login,api_password):
                 'loginMode': 1}
     r = requests.post(base_url + 'auth/login', data=data_get)
     if r.ok:
-        auth_token = r.headers['X-MSTR-AuthToken']
+        authToken = r.headers['X-MSTR-AuthToken']
         cookies = dict(r.cookies)
-        print("Token: " + auth_token)
-        return auth_token, cookies
+        print("Token: " + authToken)
+        return authToken, cookies
     else:
         print("HTTP %i - %s, Message %s" % (r.status_code, r.reason, r.text))
 
-def get_sensor_data():
-    acceleration = sense.get_accelerometer_raw()
-    gyro = sense.get_gyroscope_raw()
-    print('[{ "timestamp":"' + str(datetime.datetime.now()) +'","driverId":"' + driverId +'","accel_X":' + str(acceleration.get("x")) +',"accel_Y":' + str(acceleration.get("y")) +',"accel_Z":' + str(acceleration.get("z")) + ',"gyro_X":' + str(gyro.get("x")) +',"gyro_Y":' + str(gyro.get("y")) +',"gyro_Z":' + str(gyro.get("z")) +',"humidity":' + str(sense.get_humidity()) +',"pressure":' + str(sense.get_pressure()) +',"temperature":' + str(sense.get_temperature()) +' }]')
-    return '[{ "timestamp":"' + str(datetime.datetime.now()) +'","driverId":"' + driverId +'","accel_X":' + str(acceleration.get("x")) +',"accel_Y":' + str(acceleration.get("y")) +',"accel_Z":' + str(acceleration.get("z")) + ',"gyro_X":' + str(gyro.get("x")) +',"gyro_Y":' + str(gyro.get("y")) +',"gyro_Z":' + str(gyro.get("z")) +',"humidity":' + str(sense.get_humidity()) +',"pressure":' + str(sense.get_pressure()) +',"temperature":' + str(sense.get_temperature()) +' }]'
+def get_sensor_data(driverId):
+	acceleration = sense.get_accelerometer_raw()
+	gyro = sense.get_gyroscope_raw()
+	print('Driver ID: ' + driverId)
+	print('[{ "timestamp":"' + str(datetime.datetime.now()) +'","driverId":"' + driverId +'","accel_X":' + str(acceleration.get("x")) +',"accel_Y":' + str(acceleration.get("y")) +',"accel_Z":' + str(acceleration.get("z")) + ',"gyro_X":' + str(gyro.get("x")) +',"gyro_Y":' + str(gyro.get("y")) +',"gyro_Z":' + str(gyro.get("z")) +',"humidity":' + str(sense.get_humidity()) +',"pressure":' + str(sense.get_pressure()) +',"temperature":' + str(sense.get_temperature()) +' }]')
+	return '[{ "timestamp":"' + str(datetime.datetime.now()) +'","driverId":"' + driverId +'","accel_X":' + str(acceleration.get("x")) +',"accel_Y":' + str(acceleration.get("y")) +',"accel_Z":' + str(acceleration.get("z")) + ',"gyro_X":' + str(gyro.get("x")) +',"gyro_Y":' + str(gyro.get("y")) +',"gyro_Z":' + str(gyro.get("z")) +',"humidity":' + str(sense.get_humidity()) +',"pressure":' + str(sense.get_pressure()) +',"temperature":' + str(sense.get_temperature()) +' }]'
 
-def push_data(update_policy, auth_token, cookies, project_id, dataset_id, table_id):
-    raw_data = get_sensor_data()
+def push_data(updatePolicy, auth_token, cookies, project_id, datasetId, tableId, driverId):
+    raw_data = get_sensor_data(driverId)
     pushed_data = base64.b64encode(bytes(raw_data, 'utf-8')).decode('ascii')
-    push_url = base_url + "datasets/" + dataset_id + "/tables/" + table_id
+    push_url = base_url + "datasets/" + datasetId + "/tables/" + tableId
     headers_push = {'X-MSTR-AuthToken': auth_token,
                     'Content-Type': 'application/json',#IMPORTANT!
                     'X-MSTR-ProjectID': project_id,
-                    'updatePolicy': update_policy
+                    'updatePolicy': updatePolicy
                     }
     insert_data = '{ "name": "sensor_data_table","columnHeaders": [{"name": "timestamp","dataType": "STRING"},{"name": "driverId","dataType": "STRING"},{"name": "accel_X","dataType": "DOUBLE"},{"name": "accel_Y","dataType": "DOUBLE"},{"name": "accel_Z","dataType": "DOUBLE"},{"name": "gyro_X","dataType": "DOUBLE"},{"name": "gyro_Y","dataType": "DOUBLE"},{"name": "gyro_Z","dataType": "DOUBLE"},{"name": "humidity","dataType": "DOUBLE"},{"name": "pressure","dataType": "DOUBLE"},{"name": "temperature","dataType": "DOUBLE"}], "data":"' + pushed_data + '"}'
                    
@@ -60,15 +55,27 @@ def push_data(update_policy, auth_token, cookies, project_id, dataset_id, table_
         print("HTTP %i - %s, Message %s" % (r.status_code, r.reason, r.text))
     else:
         print("HTTP %i - %s, Message %s" % (r.status_code, r.reason, r.text))
-        
-def main():
+
+def stop_data_push():
+    print('Stop: ' + str(stop_run))
+    stop_run = True
+    print('Stop: ' + str(stop_run))
+
+def trigger(user_id,sense):
+    print('Begin pushing data for user: ' + user_id)
     i = 0;
-    while True:
+    stop_run=False
+    while stop_run != True:
+        print('Trigger run: ' + str(stop_run))
         if i > 12 or i == 0:
             token, cookies = login(base_url,api_login,api_password)
             i = 0
-        time.sleep(5)
-        push_data("Upsert", token, cookies, project_id, datasetId, tableId)
+        time.sleep(2)
+        push_data("Upsert", token, cookies, project_id, datasetId, tableId, user_id)
         i = i + 1
-
-main()
+            # stopping the run
+        for event in sense.stick.get_events():
+            if event.action == 'released' and event.direction == 'left':
+                stop_run=True
+                print('Trigger run: ' + str(stop_run))
+                print('Break out of Trigger')
